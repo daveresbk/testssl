d9230dcdec4d689fd6dc5c5001570384bafc0e1a
davidverdejobluekiri <david.verdejo@bluekiri.com>
2018-01-14 19:55:31 +0100

    remove log config from wsgi

--- wsgi.py
+++ wsgi.py
@@ -8,11 +8,4 @@ import logging
 #-----------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    logHandler = RotatingFileHandler('info.log', maxBytes=1000, backupCount=10)
-    # set the log handler level
-    logHandler.setLevel(logging.INFO)
-    # set the app logger level
-    app.logger.setLevel(logging.INFO)
-    app.logger.addHandler(logHandler)    
-    app.secret_key = 'traveltool'
     app.run()
